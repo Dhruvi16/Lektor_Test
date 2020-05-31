d69e62eac902ec99c27f3d681c9200c91fbfd88d
dhruvi16 <dhruvibutti9477@gmail.com>
2020-05-31 19:11:44 +0530

    Test deployment

---
+++ CC_Open_Source/README.md
@@ -0,0 +1,218 @@
+# creativecommons.github.io-source
+
+Source for `creativecommons.github.io`
+
+
+## Overview
+
+> **:warning: DO *NOT* MAKE CHANGES TO THE
+> [creativecommons/creativecommons.github.io][ccghiorepo] REPO DIRECTLY**.
+
+[ccghiorepo]:https://github.com/creativecommons/creativecommons.github.io
+
+This site is built using [Lektor][lektor]. All changes to
+[opensource.creativecommons.org][ccopensource]
+([creativecommons.github.io][ccgithubio]) must be made **here** and deployed
+via lektor (see [Deployment](#deployment), below).
+
+[lektor]: https://www.getlektor.com/
+[ccgithubio]: https://creativecommons.github.io/
+[ccopensource]: https://opensource.creativecommons.org/
+
+
+## Code of Conduct
+
+[`CODE_OF_CONDUCT.md`](CODE_OF_CONDUCT.md):
+> The Creative Commons team is committed to fostering a welcoming community.
+> This project and all other Creative Commons open source projects are governed
+> by our [Code of Conduct][code_of_conduct]. Please report unacceptable
+> behavior to [conduct@creativecommons.org](mailto:conduct@creativecommons.org)
+> per our [reporting guidelines][reporting_guide].
+
+[code_of_conduct]: https://creativecommons.github.io/community/code-of-conduct/ "CC Open Source Code of Conduct — Creative Commons on GitHub"
+[reporting_guide]: https://creativecommons.github.io/community/code-of-conduct/enforcement/ "Codes of Conduct Enforcement — Creative Commons on GitHub"
+
+
+## Contributing
+
+- See [`CONTRIBUTING.md`](CONTRIBUTING.md) for information on how to contribute
+- See [Contributors to
+  creativecommons/creativecommons.github.io-source][contributors] for a
+  list of authors and their contributions to this project :blush:
+
+[contributors]: https://github.com/creativecommons/creativecommons.github.io-source/graphs/contributors "Contributors to creativecommons/creativecommons.github.io-source"
+
+
+## Installation
+
+### Pre-Requisites
+
+Make sure you have [pipenv][pipenvdocs], [Node.js][nodejswebsite] and [npm][npmdocs] installed. To install these, execute the following commands:
+
+> For  macOS:
+1. `brew install pipenv`
+2. `brew install node`
+
+> For GNU/Linux:
+1. `curl https://raw.githubusercontent.com/kennethreitz/pipenv/master/get-pipenv.py | python`
+2. `sudo apt update`
+3. `sudo apt install nodejs`
+4. `sudo apt install npm `
+
+[pipenvdocs]:https://pipenv.readthedocs.io/en/latest/
+[nodejswebsite]:https://nodejs.org/en/
+[npmdocs]:https://docs.npmjs.com/ 
+
+
+### Installing Project Requirements
+
+1. Clone this repository.
+2. Open your command line interface and `cd` to the repository root directory.
+3. Run `pipenv install` to create a Python virtual environment and install the requirements for this project.
+
+
+### pipenv Troubleshooting
+
+`pipenv` doesn't always provide the best error messages ([Provide better error
+message if the project’s virtual environment is broken][pipenverror]). If all
+else fails, try removing the virtual environment and reinstalling:
+1. `pipenv --rm`
+2. `pipenv install`
+
+[pipenverror]:https://github.com/pypa/pipenv/issues/1918
+
+
+## Development
+
+- Run `pipenv run lektor server -f webpack` to start the Lektor development
+  server.
+- You will be able to see the website at [`http://localhost:5000/`][local5000].
+  - The Lektor server will rebuild the site every time you change any content.
+
+[local5000]:http://localhost:5000/
+
+
+## Deployment
+
+We have continuous deployment set up. To deploy, push your code to the `master`
+branch (or make a pull request against the `master` branch. GitHub Actions
+builds and deploys the site whenever it detects new commits on the `master`
+branch.
+
+The GitHub Actions configuration is located at
+[`.github/workflows/lektor-build-deploy.yml`][lektorbuild].
+
+[lektorbuild]: .github/workflows/lektor-build-deploy.yml
+
+
+### Manual Deployment
+
+> :warning: **For reference only, you should not need to not do this.**
+
+When you are ready to deploy a new version of the site, run `lektor deploy`
+(assuming you have your GitHub SSH key already set up and you have access to
+the [creativecommons/creativecommons.github.io][ccghiorepo] repository). That's
+it, it's live on production!
+
+
+## Project Structure
+
+Here's how the code is structured in the top level of the repository:
+- **`assets`**: This directory contains the JavaScript and CSS files for the
+  project built via webpack. Most of the JavaScript and CSS is third-party code
+  and loaded via CDN so this is pretty empty.
+- **`content`**: The content of the site lives here. [Here's an explanation of
+  how content works in Lektor][lektorcontent]. This is probably what you'll be
+  modifying most often.
+- **`models`**: [All content in Lektor is associated with data
+  models][lektormodels] to define their schema. Currently, we only use the
+  default `page` model that ships with Lektor.
+- **`templates`**: This is where the [Jinja2][jinja2] templates that render
+  content are stored. See the [Lektor template documentation][lektortemplate]
+  for more information.
+- **`webpack`**: This is where all the webpack config files as well as Sass and
+  JavaScript files for the project resides. The JavaScript and Sass files are
+  compiled and saved in the assets folder during lektor build process.
+
+[lektorcontent]:https://www.getlektor.com/docs/content/
+[lektormodels]:(https://www.getlektor.com/docs/models/
+[jinja2]:http://jinja.pocoo.org/
+[lektortemplate]:https://www.getlektor.com/docs/templates/
+
+
+### Lektor Plugins
+
+- [Lektor Plugins][plugins]:
+  - [nixjdm/lektor-atom][atom]: Lektor Atom plugin
+  - [lektor/lektor-disqus-comments][disqus]: Adds disqus comments to a Lektor
+    website
+  - [kmonsoor/lektor-google-analytics][lektorga]: Integration of Google
+    analytics with Lektor CMS
+  - [bancek/lektor-markdown-excerpt][mdexcerpt]: Adds filter for Markdown body
+    excerpt
+  - [lektor/lektor-markdown-header-anchors][md-header]: Adds support for
+    anchors and table of contents to Markdown
+  - [lektor/lektor-markdown-highlighter][md-highlighter]: Adds support for 
+    syntax highlighting in Markdown and templates
+  - [terminal-labs/lektor-strip-html-tags][striphtml]: Strip HTML tags,
+    effectively turning HTML into plain text
+  - [lektor/lektor-webpack-support][webpacksupport]: Adds webpack support to
+    lektor
+
+[plugins]: https://www.getlektor.com/docs/plugins/
+[atom]: https://github.com/nixjdm/lektor-atom
+[disqus]: https://github.com/lektor/lektor-disqus-comments
+[lektorga]: https://github.com/kmonsoor/lektor-google-analytics
+[mdexcerpt]: https://github.com/bancek/lektor-markdown-excerpt
+[md-header]: https://github.com/lektor/lektor-markdown-header-anchors
+[md-highlighter]: https://github.com/lektor/lektor-markdown-highlighter
+[striphtml]: https://github.com/terminal-labs/lektor-strip-html-tags
+[webpacksupport]: https://github.com/lektor/lektor-webpack-support
+
+
+## Redirects
+
+- `/cc-vocabulary/` to https://cc-vocabulary.netlify.com/
+  - Added so that the `opensource.creativecommons.org/cc-vocabulary/` will
+    continue to work with that project moving to utilize Netlify.
+- `/cc-vue-vocabulary` to https://cc-vue-vocabulary.netlify.com/
+  - Added so that the `opensource.creativecommons.org/cc-vue-vocabulary/` will
+    continue to work with that project moving to utilize Netlify.
+- `/cc-fonts` to https://cc-fonts.netlify.com/
+  - Added so that the `opensource.creativecommons.org/cc-fonts/` will
+    continue to work with that project moving to utilize Netlify.
+
+
+## License
+
+
+### Code
+
+- [`LICENSE`](LICENSE) (Expat/[MIT][mit] License)
+
+[mit]: http://www.opensource.org/licenses/MIT "The MIT License | Open Source Initiative"
+
+
+### Content
+
+![CC BY 4.0 license button][cc-by-png]
+
+All the content within this repository is licensed under a [Creative Commons 
+Attribution 4.0 International License][cc-by] unless otherwise
+specified.
+
+[cc-by-png]: https://licensebuttons.net/l/by/4.0/88x31.png "CC BY 4.0 license button"
+[cc-by]: https://creativecommons.org/licenses/by/4.0/ "Creative Commons — Attribution-ShareAlike 4.0 International — CC BY 4.0"
+
+
+### Font Awesome
+
+This repository contains PNG icons that were converted from Font Awesome SVGs:
+
+> Font Awesome Free is free, open source, and GPL friendly. You can use it for
+> commercial projects, open source projects, or really almost whatever you
+> want. (Full Font Awesome Free license: https://fontawesome.com/license/free)
+
+> **Icons:** CC BY 4.0 License (https://creativecommons.org/licenses/by/4.0/)
+> In the Font Awesome Free download, the CC BY 4.0 license applies to all icons
+> packaged as SVG and JS file types.
---
+++ CC_Open_Source/assets/static/gen/script.js
@@ -0,0 +1,133 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./js/ideas.js":
+/*!*********************!*\
+  !*** ./js/ideas.js ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("$(document).ready(function(){\n    // Class for active buttons\n    const active_class = 'btn-dark';\n\n    // Class for inactive buttons\n    const inactive_class = 'btn-primary';\n\n    // jQuery object containing all the filter buttons\n    const filter_btn = $('.filter-btn');\n\n    // Animation delay for show() function\n    const animation_delay = 100;\n\n    // List of Available filters\n    const filters = [\n      'all',\n      'difficulty-easy',\n      'difficulty-medium',\n      'difficulty-hard',\n      'skill-javascript',\n      'skill-python',\n      'skill-wordpress',\n      'skill-writing'\n    ];\n\n    // Variable to hold selected filters\n    var selected = [];\n\n    // To display selected filters\n    function showIdeas(selected)\n    {\n        // If no filter is selected/show all\n      if(selected.length == 0){\n        $('.filter').show(animation_delay);\n        return selected;\n      }\n        // Get filters which were not selected\n      hidden = filters.filter(n => !selected.includes(n));\n\n        // Hide not selected\n      for (const idea in hidden) {\n        if (hidden.hasOwnProperty(idea)) {\n          const element = hidden[idea];\n          $('.filter.' + element).hide();\n        }\n      }\n        // Show not selected\n      for (const idea in selected) {\n        if (selected.hasOwnProperty(idea)) {\n          const element = selected[idea];\n          $('.filter.' + element).show(animation_delay);\n        }\n      }\n      return selected;\n    }\n\n    // To reset the selected filters / All Projects button\n    function resetFilters()\n    {\n      $('.filter-btn[data-filter=\"all\"]').removeClass(inactive_class).addClass(active_class);\n      return filter_btn.not('[data-filter=\"all\"]').removeClass(active_class).addClass(inactive_class);\n    }\n\n    // To toggle classes in filter buttons\n    function toggleBtn(btn)\n    {\n      if(btn.hasClass(active_class)){\n       return btn.removeClass(active_class).addClass(inactive_class);\n      }\n      return btn.removeClass(inactive_class).addClass(active_class);\n    }\n\n    // Click event handler\n    filter_btn.click(function() {\n      var value = $(this).data('filter');\n      // Reset all filters and show all\n      if(value == 'all')\n      {\n        selected = [];\n        resetFilters();\n        return showIdeas(selected);\n      }\n      if(selected.indexOf(value) > -1)\n      {\n        // If the filter is already selected, remove it from selected and toggle buttons\n        selected.splice(selected.indexOf(value), 1);\n      }else if(filters.indexOf(value) > -1) {\n        // If the filter is not selected, add it to the selected array and toggle buttons\n        $('.filter-btn[data-filter=\"all\"]').removeClass(active_class).addClass(inactive_class);\n        selected.push(value);\n      }\n      toggleBtn($(this));\n      return showIdeas(selected);\n    });\n  });\n\n//# sourceURL=webpack:///./js/ideas.js?");
+
+/***/ }),
+
+/***/ "./js/main.js":
+/*!********************!*\
+  !*** ./js/main.js ***!
+  \********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("$(document).ready(function () {\n    $(window).scroll(function () {\n        if ($(this).scrollTop() > 500) {\n            $('#back-to-top').fadeIn();\n        } else {\n            $('#back-to-top').fadeOut();\n        }\n    });\n\n    patchAssetIntoDom('/assets/logos/cc/logomark.svg');\n    patchAssetIntoDom('/assets/logos/cc/letterheart.svg');\n    patchAssetIntoDom('/assets/logos/products/open_source.svg');\n\n    // Check for click events on the navbar burger icon\n    $(\".navbar-burger\").click(function() {\n        // Toggle the \"is-active\" class on both the \"navbar-burger\" and the \"navbar-menu\"\n        $(\".navbar-burger\").toggleClass(\"is-active\");\n        $(\".navbar-menu\").toggleClass(\"is-active\");\n    });\n\n    $(\".navbar-item.has-dropdown\").click(function() {\n        if ($(\".navbar-burger\").is(':visible')) {\n            $(this).toggleClass(\"is-active\");\n        }\n    });\n    $(\".navbar-item > .navbar-link\").click(function(e) {\n        if ($(\".navbar-burger\").is(':visible')) {\n            e.preventDefault();\n        }\n    });\n    $(window).resize(function() {\n        if (!$(\".navbar-burger\").is(':visible') && $(\".navbar-item.has-dropdown.is-active\").length) {\n            $(\".navbar-item.has-dropdown.is-active\").removeClass('is-active');\n        }\n    });\n});\n\nconst getFullyQualifiedUrl = (path, version) => {\n    let baseUrl = \"https://unpkg.com/@creativecommons/vocabulary\"\n    if (version) {\n        baseUrl = `${baseUrl}@${version}`\n    }\n    return `${baseUrl}/${path}`\n}\n\nconst patchAssetIntoDom = (asset, version = null) => {\n    const ajax = new XMLHttpRequest();\n    ajax.open(\"GET\", getFullyQualifiedUrl(asset, version), true);\n    ajax.onload = () => {\n        var div = document.createElement(\"div\");\n        // Render SVG in the page\n        div.innerHTML = ajax.responseText;\n        div.style.display = 'none';\n        document.body.insertBefore(div, document.body.childNodes[0]);\n    }\n\n    ajax.send();\n}\n\n\n\n//# sourceURL=webpack:///./js/main.js?");
+
+/***/ }),
+
+/***/ "./sass/main.scss":
+/*!************************!*\
+  !*** ./sass/main.scss ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"style.css\";\n\n//# sourceURL=webpack:///./sass/main.scss?");
+
+/***/ }),
+
+/***/ 0:
+/*!*********************************************************!*\
+  !*** multi ./js/main.js ./js/ideas.js ./sass/main.scss ***!
+  \*********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("__webpack_require__(/*! ./js/main.js */\"./js/main.js\");\n__webpack_require__(/*! ./js/ideas.js */\"./js/ideas.js\");\nmodule.exports = __webpack_require__(/*! ./sass/main.scss */\"./sass/main.scss\");\n\n\n//# sourceURL=webpack:///multi_./js/main.js_./js/ideas.js_./sass/main.scss?");
+
+/***/ })
+
+/******/ });
